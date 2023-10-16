62728166e1fbfa027056a2e989d17a7110525203
Bijayshah2002 <121917794+Bijayshah2002@users.noreply.github.com>
2023-10-16 17:56:52 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 001
+let accountEmail="sethr336@gmail.com"
+var accountPassword="12345"
+accountCity="Birgunj"  //variables can be also decleared in this way but not prefered.
+let accountState;
+// accountId = 2   Not Allowed
+/*
+prefer not to use var because of issue of block code and functional code
+*/
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountState])
+
