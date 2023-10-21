31bac2c0dc38604809067f1b2d36ef289353474d
Bijayshah2002 <121917794+Bijayshah2002@users.noreply.github.com>
2023-10-21 04:22:58 +0000

    var,let,const,local and global scope

---
+++ 03_Basics/scope.js
@@ -0,0 +1,24 @@
+// concept of var,let and const
+let  a= 1
+var  b=2,b1=21
+const c=3
+console.log("a :",a)
+console.log("b :",b);
+console.log("b1 :",b1);
+console.log("c :",c)
+
+if(true){
+let a="bijay"
+var b="shah"
+b1="21 check"// also treated as let
+const c="no middelname"
+console.log("a :",a)
+console.log("b :",b);
+console.log("b1 :",b1);
+console.log("c :"+c)
+}
+
+console.log("a :",a)
+console.log("b :",b);
+console.log("b1 :",b1);
+console.log("c :",c)
