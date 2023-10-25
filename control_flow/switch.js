513491e6fb3ac2c9024e1f6bad090aacc9f2c8f5
Bijayshah2002 <121917794+Bijayshah2002@users.noreply.github.com>
2023-10-25 05:27:37 +0000

    all control flows

---
+++ control_flow/switch.js
@@ -0,0 +1,34 @@
+
+// switch(key)
+// case value:
+// // statement
+// break;
+// case value:
+//     // statement
+//     break;
+
+const month=6
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+    case 5:
+        console.log("May")
+        break;
+    case 6:
+        console.log("june")
+        break;
+
+    default:
+        console.log("not a month")
+        break;
+}
