439abe216926d7f5f49eb09d99c59cbf3dcb6da4
Bijayshah2002 <121917794+Bijayshah2002@users.noreply.github.com>
2023-10-30 03:22:00 +0000

    about while and do while loops

---
+++ 05_Iterations/whileDoWhile.js
@@ -0,0 +1,20 @@
+// while loops
+let i=0
+while(i<10){
+    console.log('value of index is :'+i)
+    i+=2
+}
+// while loop in array
+let myArr=['flash','batman',"superman"]
+ i=0
+while(i<myArr.length){
+    console.log("value is "+myArr[i])
+    i++
+}
+
+// do while loop....prints at leaast once even the condition is not true. 
+let Score=0
+do {
+    console.log('score is '+Score)
+    Score++
+} while(Score<=10)
